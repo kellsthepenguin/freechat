f1a089f23976618c1c08bacc19a716b258a9cc78
Kim Jioo <jwkim101201@gmail.com>
2021-05-11 15:25:16 +0900

    feat: added token getting
    근데 이제 작동을 안하는

--- router/api.js
+++ router/api.js
@@ -2,6 +2,25 @@ const { Router } = require('express')
 
 const router = Router()
 
+router.get('/token', (req, res) => {
+  const {
+    mail, 
+    pw
+  } = req.body
+
+  if (!mail && pw) {
+    return res.json({
+      error: 401,
+      msg: 'Unauthorized',
+      token: null
+    })
+  }
+
+  res.json({
+    token: '이것도 준비 안됐다고.'
+  })
+})
+
 router.get('/groups', (req, res) => {
   res.send('으쯔라고요 준비 안됐다고.')
 })
